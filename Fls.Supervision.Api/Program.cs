03a77242a06d82f9a3dc1d3e12bdedd4d6261b8e
Vitaly Kamiansky <vitaly.kamiansky@firstlinesoftware.com>
2020-05-24 18:59:33 +0300

    First integration test
    Added test request groundwork

--- Fls.Supervision.Api/Program.cs
+++ Fls.Supervision.Api/Program.cs
@@ -8,23 +8,31 @@ using Convey.Logging;
 using Convey.MessageBrokers.RabbitMQ;
 using Convey.WebApi;
 using Convey.WebApi.CQRS;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Hosting;
 using Convey.CQRS.Commands;
 using Fls.Supervision.Api.Commands;
+using Fls.Supervision.Api.Providers;
+using Fls.Supervision.Api.Providers.Implementations;
+using System.Text.Json;
 
 namespace Fls.Supervision.Api
 {
-    public class Program
+    public static class Extensions
     {
-        public static Task Main(string[] args)
-            => CreateHostBuilder(args).Build().RunAsync();
+        public static async Task OkAsync<T>(this HttpResponse response, T value)
+        {
+            var serialized = JsonSerializer.Serialize<T>(value, new JsonSerializerOptions { WriteIndented = true });
+            await response.WriteAsync(serialized);
+        }
 
-        public static IHostBuilder CreateHostBuilder(string[] args)
-            => Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(webBuilder =>
-            {
-                webBuilder.ConfigureServices(services => services
+        public static IWebHostBuilder UseSupervisionApi(this IWebHostBuilder webBuilder)
+        {
+            webBuilder
+                    .ConfigureServices(services => services
+                        .AddScoped<IStorageProvider, InMemoryStorageProvider>()
                         .AddConvey()
                         //.AddConsul()
                         //.AddFabio()
@@ -40,9 +48,19 @@ namespace Fls.Supervision.Api
                             .Get("", ctx => ctx.Response.WriteAsync("FLS Supervision API"))
                             .Get("ping", ctx => ctx.Response.WriteAsync("pong"))
                             .Get("query/{query_data}", ctx => ctx.Response.WriteAsync("здесь отвечаем на запросы"))
-                            .Post<ProcessGithubEvent>("webhook", afterDispatch: (cmd, ctx) => ctx.Response.Ok())))
-                        //.UseRabbitMq())
+                            .Post<ProcessGithubEvent>("webhook", afterDispatch: (cmd, ctx) => ctx.Response.OkAsync(new { Hook = cmd.Hook, Message = "Event accepted." }))))
+                    //.UseRabbitMq())
                     .UseLogging();
-            });
+
+            return webBuilder;
+        }
+    }
+    public class Program
+    {
+        public static Task Main(string[] args)
+            => CreateHostBuilder(args).Build().RunAsync();
+
+        public static IHostBuilder CreateHostBuilder(string[] args)
+            => Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(webBuilder => webBuilder.UseSupervisionApi());
     }
 }
--- Fls.Supervision.Test.Integration/SupervisionApiTests.cs
+++ Fls.Supervision.Test.Integration/SupervisionApiTests.cs
@@ -1,7 +1,5 @@
-using System;
 using Fls.Supervision.Api;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.TestHost;
+using Fls.Supervision.Test.Integration.Helpers;
 using Xunit;
 
 namespace Fls.Supervision.Test.Integration
@@ -9,15 +7,12 @@ namespace Fls.Supervision.Test.Integration
     public class SupervisionApiTests
     {
         [Fact]
-        public void PingTest ()
+        public async void PingTest()
         {
-            //var testServer = new TestServer(Program.CreateHostBuilder(new string[] {}));
-            
-         
-            //Где-то здесь тестовый хост, но не очень круто
-            //код - сила
-            //я бы что-то написал, но у меня нет подсказок(
-             //   var test = new TestServer(WebHostBuilder.)
+            var server = TestHelper.CreateTestServer(webHostBuilder => webHostBuilder.UseSupervisionApi());
+            var client = TestHelper.CreateTestClient(server);
+            var result = await client.GetAsync(() => TestEndpoints.PingGet);
+            Assert.Equal("pong", result);
         }
     }
 }
