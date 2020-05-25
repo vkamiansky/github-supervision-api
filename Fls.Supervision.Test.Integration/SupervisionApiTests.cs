7be32d48ad24409fe7bf880869e2b8108f13af20
Valeria Samonina <valeriya.samonina@list.ru>
2020-05-25 18:02:09 +0300

    Добавлен интеграционный тест на вебхук

--- Fls.Supervision.Test.Integration/SupervisionApiTests.cs
+++ Fls.Supervision.Test.Integration/SupervisionApiTests.cs
@@ -1,4 +1,6 @@
+using System;
 using Fls.Supervision.Api;
+using Fls.Supervision.Api.Commands;
 using Fls.Supervision.Test.Integration.Helpers;
 using Xunit;
 
@@ -12,6 +14,27 @@ namespace Fls.Supervision.Test.Integration
             var server = TestHelper.CreateTestServer(webHostBuilder => webHostBuilder.UseSupervisionApi());
             var client = TestHelper.CreateTestClient(server);
             var result = await client.GetAsync(() => TestEndpoints.PingGet);
+            Assert.Equal("pong", result);
+        }
+
+        [Fact]
+        public async void PingTest2()
+        {
+            var server = TestHelper.CreateTestServer(webHostBuilder => webHostBuilder.UseSupervisionApi());
+            var client = TestHelper.CreateTestClient(server);
+            var testWebHookPayload = new ProcessGithubEvent {
+                Zen = "Anything added dilutes everything else.",
+                HookId = 109948940, 
+                CreatedAt = DateTime.Now,
+                Hook = new ProcessGithubEvent.HookPayload{
+                    Ig = 109948940,
+                    Type = "Repository", 
+                    Name = "web"
+                }
+
+            };
+            var result = await client.PostJsonAsync(TestEndpoints.WebHookPost, testWebHookPayload);
+            System.Console.WriteLine(result);
             Assert.Equal("pong", result);
         }
     }
--- Fls.Supervision.Test.Integration/TestEndpoints.cs
+++ Fls.Supervision.Test.Integration/TestEndpoints.cs
@@ -3,5 +3,7 @@ namespace Fls.Supervision.Test.Integration
     public static class TestEndpoints
     {
         public const string PingGet = "ping";
+
+        public const string WebHookPost = "webhook";
     }
 }
