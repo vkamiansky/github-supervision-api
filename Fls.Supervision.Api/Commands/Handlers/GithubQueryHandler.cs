5bdf7473013554bb8ead0ac991ba5eabc6805ce9
TheSplendidOne <k.sinichenko@yandex.by>
2020-05-27 19:34:14 +0300

    Some clean up

--- Fls.Supervision.Api/Commands/Handlers/GithubQueryHandler.cs
+++ Fls.Supervision.Api/Commands/Handlers/GithubQueryHandler.cs
@@ -13,7 +13,7 @@ namespace Fls.Supervision.Api.Commands.Handlers
 
     public class GithubQueryHandler : IQueryHandler<GithubQuery, List<PullRequestRecordData>>
     {
-        private readonly static PullRequestRecordRepository repository = new PullRequestRecordRepository(null, null);   //[TODO] разобраться с репозиторием
+        private static readonly PullRequestRecordRepository repository = new PullRequestRecordRepository(null, null);   //[TODO] разобраться с репозиторием
 
         public async Task<List<PullRequestRecordData>> HandleAsync(GithubQuery query)
         {
@@ -23,44 +23,27 @@ namespace Fls.Supervision.Api.Commands.Handlers
                 {
                     if (await repository.ExistsAsync(e => e.Id == query.queryData.Id))
                     {
-                        var t = await repository.GetAsync(query.queryData.Id.Value);
-                        return new List<PullRequestRecordData>() { t };
+                        return new List<PullRequestRecordData>() { await repository.GetAsync(query.queryData.Id.Value) };
                     }
-                    else return null;
+                    return null;
                 }
-                else
+
+                Func<PullRequestRecordData, bool> t = e =>
                 {
-                    Func<PullRequestRecordData, bool> t = e =>
-                    {
-                        if (query.queryData.DelayHistory != null)
-                        {
-                            if (query.queryData.DelayHistory != e.DelayHistory) return false;
-                        }
-                        if (query.queryData.GapHistory != null)
-                        {
-                            if (query.queryData.GapHistory != e.GapHistory) return false;
-                        }
-                        if (query.queryData.StateHistory != null)
-                        {
-                            if (query.queryData.StateHistory != e.StateHistory) return false;
-                        }
-                        if (query.queryData.LastCommitDate.HasValue)
-                        {
-                            if (query.queryData.LastCommitDate != e.LastCommitDate) return false;
-                        }
-                        if (query.queryData.LastReviewCommentDate.HasValue)
-                        {
-                            if (query.queryData.LastReviewCommentDate != e.LastReviewCommentDate) return false;
-                        }
-                        return true;
-                    };
-                    if (query.elementsPerPageNumber.HasValue)
-                    {
-                        query.pageNumber ??= 50;
-                        return ((List<PullRequestRecordData>)await repository.FindAsync(e => t(e))).GetRange((query.pageNumber.Value - 1) * query.elementsPerPageNumber.Value, query.elementsPerPageNumber.Value);
-                    }
-                    else return (List<PullRequestRecordData>)await repository.FindAsync(e => t(e));
+                    if (query.queryData.DelayHistory?.SequenceEqual(e.DelayHistory) ?? false) return false;
+                    if (query.queryData.GapHistory?.SequenceEqual(e.GapHistory) ?? false) return false;
+                    if (query.queryData.StateHistory?.SequenceEqual(e.StateHistory) ?? false) return false;
+                    if (query.queryData.LastCommitDate != e.LastCommitDate) return false;
+                    return query.queryData.LastReviewCommentDate == e.LastReviewCommentDate;
+                };
+                if (query.elementsPerPageNumber.HasValue)
+                {
+                    query.pageNumber ??= 50;
+                    return ((List<PullRequestRecordData>) await repository.FindAsync(e => t(e))).GetRange(
+                        (query.pageNumber.Value - 1) * query.elementsPerPageNumber.Value,
+                        query.elementsPerPageNumber.Value);
                 }
+                return (List<PullRequestRecordData>) await repository.FindAsync(e => t(e));
             }
             return null;
         }
